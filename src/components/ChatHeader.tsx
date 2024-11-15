3b2a90dc7c8e25140eb4110c80950b98a9228070
Ngo-Bassom-Anne-Rosa <nbarprincesse@gmail.com>
2024-11-15 16:48:59 +0100

    added

--- src/components/ChatHeader.tsx
+++ src/components/ChatHeader.tsx
@@ -1,28 +1,23 @@
 import React from 'react';
 import { FaEllipsisV, FaUser } from 'react-icons/fa';
-import { RadioGroup, Radio } from '@/components/ui/radio';
 
 const ChatHeader: React.FC<{ contactName: string, contactStatus: string }> = ({ contactName, contactStatus }) => {
  return (
    <div className="h-16 px-4 flex items-center justify-between">
      <div className="flex items-center gap-4">
-       <FaUser className="w-10 h-10 rounded-full" />
+       <FaUser className="w-10=h-10 rounded-full" />
        <div>
          <h3>{contactName}</h3>
          <span className="text-xs">{contactStatus}</span>
        </div>
      </div>
      <div className="flex items-center gap-4">
-       <RadioGroup>
-         <Radio value="option1" defaultChecked>
-           Option 1
-         </Radio>
-         <Radio value="option2">
-           Option 2
-         </Radio>
-       </RadioGroup>
-       <FaEllipsisV />
-     </div>
+        <label className="inline-flex items-center">
+          <input type="radio" className="form-radio" name="option" value="option1" checked />
+          <span className="ml-2">Option 1</span>
+        </label>
+        <FaEllipsisV />
+      </div>
    </div>
  );
 };
