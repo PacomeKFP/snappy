73dfe9d9995e1ceaf41b87dec324a1a456fb33a7
Ngo-Bassom-Anne-Rosa <151435188+Ngo-Bassom-Anne-Rosa@users.noreply.github.com>
2025-01-26 12:32:20 +0000

    worked on navabar issue

---
+++ src/app/developpers/page.tsx
@@ -0,0 +1,20 @@
+import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
+
+import { Metadata } from "next";
+
+
+
+const Developpers = () => {
+    return (
+      
+        <div className="mx-auto w-full max-w-[1080px]">
+          <Breadcrumb pageName="Developpers" />
+  
+          
+        </div>
+      
+    );
+  };
+  
+  export default Developpers;
+  
