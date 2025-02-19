92cb9b6330b5bcdc650bf883eb5fac18a6696592
PacomeKFP <pacomekengafe@gmail.com>
2025-02-19 18:02:54 +0100

    all is ok

--- models/message_model.py
+++ models/message_model.py
@@ -15,7 +15,7 @@ class Message(Base):
     id = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     sender = Column(String(36), nullable=False)
     receiver = Column(String(36), nullable=False)
-    body = Column(String(255000), nullable=False)
+    body = Column(String(8192), nullable=False)
     projectId = Column(String(36), nullable=False)
     isWrittenByHuman = Column(Boolean, default=False)
     mode = Column(Enum(MessagingMode , Name="messaging_mode"), nullable=False)
